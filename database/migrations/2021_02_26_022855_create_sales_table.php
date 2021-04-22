66a7f43637976b9d48c275b3be11a559bbb29aad
MartinZayasZ <mzayas@somos.mx>
2021-04-21 20:58:49 -0700

    últimos cambios en migraciones

--- database/migrations/2021_02_26_022855_create_sales_table.php
+++ database/migrations/2021_02_26_022855_create_sales_table.php
@@ -16,7 +16,7 @@ class CreateSalesTable extends Migration
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('customer_id')->constrained('users');
+            $table->foreignId('customer_id')->constrained('customers');
             $table->string('description', 100);
             $table->double('subtotal');
             $table->double('total');
